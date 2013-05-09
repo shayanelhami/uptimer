3deb1ac73ae8a5be80bf2b34307a2853af93d641
shayan <shayanelhami@gmail.com>
2013-05-09 21:46:15 +0100

    Add express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('not listening');
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.end('Hello world!');
+});
+
+
+app.listen(8085);
+console.log('listening on 8085');
