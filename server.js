bec70bfdb660ee9661414656c482a671c77e15a4
shayan <shayanelhami@gmail.com>
2013-05-09 22:39:16 +0100

    Store uptime in file, use its content for /

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 app.use(express.bodyParser());
 
+var FILE_NAME = './uptime.txt';
+
 app.get('/', function(req, res){
-  res.send('Hello world!');
+  fs.readFile(FILE_NAME, 'utf8', function(err, data) {
+	  if (err) {
+			console.log(err);
+			res.send(err);
+		} else {
+			res.send('Uptime:'+ data);
+		}
+	});
 });
 
 app.post('/save', function (req, res) {
     var post = req.body;
 	console.log(post.uptime);
-	res.send('OK!');
+	
+	fs.writeFile(FILE_NAME, post.uptime, function(err) {
+		if (err) {
+			console.log(err);
+			res.send(err);
+		} else {
+			res.send('OK!\n');
+		}
+	}); 
+
 });
 	
 app.listen(8085);
