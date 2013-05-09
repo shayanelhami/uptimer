363bddbf3f03f6ca29e1e69474ad0f9da428272c
shayan <shayanelhami@gmail.com>
2013-05-09 22:27:47 +0100

    Basic post request

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express');
 
 var app = express();
+app.use(express.bodyParser());
 
 app.get('/', function(req, res){
-  res.end('Hello world!');
+  res.send('Hello world!');
 });
 
-
+app.post('/save', function (req, res) {
+    var post = req.body;
+	console.log(post.uptime);
+	res.send('OK!');
+});
+	
 app.listen(8085);
 console.log('listening on 8085');
